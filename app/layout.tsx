94eac7e35f5fdb6bed5b19905ca5ae4bac675013
AvielSheto <avielsheto@gmail.com>
2023-02-21 23:54:38 +0200

    Google login

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,35 @@
 import { SessionProvider } from "../components/SessionProvider";
 import SideBar from "../components/SideBar";
-import {getServerSession} from "next-auth";
+import { getServerSession } from "next-auth";
 import "../styles/globals.css";
 import { authOptions } from "../pages/api/auth/[...nextauth]";
+import Login from "../components/Login";
 
 export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-const session = await getServerSession(authOptions)
+  const session = await getServerSession(authOptions);
 
   return (
     <html>
       <head />
       <body>
         <SessionProvider session={session}>
-          <div className="flex">
-            <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
-              <SideBar />
-            </div>
-            {/* {Sidebar} */}
+          {!session ? (
+            <Login />
+          ) : (
+            <div className="flex">
+              <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+                <SideBar />
+              </div>
+              {/* {Sidebar} */}
 
-            {/* {ClientProvider - notification} */}
-            <div className="bg-[#343541] flex-1">{children}</div>
-          </div>
+              {/* {ClientProvider - notification} */}
+              <div className="bg-[#343541] flex-1">{children}</div>
+            </div>
+          )}
         </SessionProvider>
       </body>
     </html>
